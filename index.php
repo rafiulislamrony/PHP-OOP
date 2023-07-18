db5d0abeb04e78efabd1d5537f8b62662478f344
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-07-18 20:55:53 +0600

    Constructor In PHP OOP

--- index.php
+++ index.php
@@ -3,28 +3,34 @@
 class Car
 {
     //properties
-    public $name = "Car Name";
-
+    public $name;
+    public $color = "Black";
+    public function __construct($car_name, $color="blue")
+    {
+        $this->name = $car_name;
+        $this->color = $color;
+    } 
     //method 
-    public function getCarName(){
+    public function getCarName()
+    {
         return $this->name;
-    }
-    public function setCarName($car_name){
-        return $this->name = $car_name;
-    }
+    } 
+    public function getCarColor()
+    {
+        return $this->color;
+    } 
 
 }
 //object 
-// $bmw = new Car();
-// $bmw->name = "BMW Car";
-// echo $bmw->name;
-// echo $bmw->getCarName();
+$bmw = new Car("BMW Car", "red"); 
+//  echo $bmw->name; 
+echo $bmw->getCarColor();
 
 // object 2nd
-$nissian = new Car();
-// echo $nissian->name;
-//echo $nissian->getCarName();
- echo $nissian->setCarName("nissian Car"); 
+// $nissian = new Car();
+// // echo $nissian->name;
+// //echo $nissian->getCarName();
+// echo $nissian->setCarName("nissian Car");
 
 
 ?>
