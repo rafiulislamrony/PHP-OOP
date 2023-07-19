6a6b576d14b2754e9ac6585d5f84c0e011f86394
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-07-19 18:07:00 +0600

    PHP Constants

--- index.php
+++ index.php
@@ -2,50 +2,15 @@
 
 class Car
 {
-    //properties
-    public $name;
-    protected $model;
-    private $color;
-    // //method 
-    public function __construct($name, $model, $color){
-        $this->name = $name;
-        $this->model = $model;
-        $this->color = $color;
-    }
-    public function enginePowerdBy(){
-        echo "Diesal";
-    }
-    final function noOfWheels(){
-        echo 4;
-    }
-    public function detail()
-    {
-        echo $this->name . '<br/>';
-        echo $this->model . '<br/>'; // 
-        echo $this->color . '<br/>'; // 
-    } 
-} 
+   const noOfWhels = "No Of Whells is 4";
+   public function getNoOfWhells(){
+    return self::noOfWhels;
+   }
 
-final class BMW{
-    public function enginePowerdBy(){
-        echo "Electric";
-    }
 }
 
-// class Tesla extends BMW{
-//     public function enginePowerdBy(){
-//         echo "Battery";
-//     }
-// }
-$BMW =new BMW();
-$BMW->enginePowerdBy();
-
-// $tesla =new Tesla('tesla', 'teslax', 'RED');
-// $tesla->noOfWheels();  
-
-
-
- 
-
+// echo $whells= Car::noOfWhels;
+$car = new Car();
+echo $car->getNoOfWhells(); 
 
 ?>
