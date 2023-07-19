d71a467aa9c78fd93e08a785c41bbf5094a59082
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-07-19 17:20:34 +0600

    PHP Inheritance

---
+++ AccessModifiers.php
@@ -0,0 +1,59 @@
+<?php
+
+class Car
+{
+    //properties
+    // public $name = "Car Name";
+    // protected $model = "Car_x";
+    // public $color = "Red";
+    // //method 
+    // public function detail()
+    // {
+    //     echo $this->name . '<br/>';
+    //     echo $this->model . '<br/>'; // 
+    //     echo $this->color . '<br/>'; // 
+    // }
+
+
+    //method
+    public function publicMethod(){
+        echo 'This is public method </br>';
+    }
+    protected function protectedMethod(){
+        echo 'This is protected method </br>';
+    }
+    private function privateMethod()
+    {
+        echo 'This is a private method </br>';
+    }
+    public function callPrivateMethod()
+    {
+       $this->privateMethod();
+    } 
+}
+//object 
+$bmw = new Car();
+
+$bmw->publicMethod();
+// $bmw->protectedMethod();
+$bmw->callPrivateMethod();
+
+
+
+
+
+
+
+
+
+
+//  $bmw->name= "Bmw Car";
+//$bmw->model= "Bmw-X";
+//  $bmw->detail();
+// echo $bmw->name . '<br/>';
+//echo $bmw->model . '<br/>'; //Error
+// echo $bmw->color . '<br/>'; //Error 
+ 
+
+
+?>
--- index.php
+++ index.php
@@ -3,58 +3,46 @@
 class Car
 {
     //properties
-    // public $name = "Car Name";
-    // protected $model = "Car_x";
-    // public $color = "Red";
+    public $name;
+    protected $model;
+    private $color;
     // //method 
-    // public function detail()
-    // {
-    //     echo $this->name . '<br/>';
-    //     echo $this->model . '<br/>'; // 
-    //     echo $this->color . '<br/>'; // 
+    public function __construct($name, $model, $color){
+        $this->name = $name;
+        $this->model = $model;
+        $this->color = $color;
+    }
+    public function enginePowerdBy(){
+        echo "Diesal";
+    }
+    public function detail()
+    {
+        echo $this->name . '<br/>';
+        echo $this->model . '<br/>'; // 
+        echo $this->color . '<br/>'; // 
+        echo $this->ownerName. '<br/>';
+    } 
+} 
+
+class Tesla extends Car{
+    public $ownerName;
+    // public function __construct($name, $model, $color, $ownerName){
+    //     $this->name = $name;
+    //     $this->model = $model;
+    //     $this->color = $color; 
     // }
-
-
-    //method
-    public function publicMethod(){
-        echo 'This is public method </br>';
-    }
-    protected function protectedMethod(){
-        echo 'This is protected method </br>';
-    }
-    private function privateMethod()
-    {
-        echo 'This is a private method </br>';
-    }
-    public function callPrivateMethod()
-    {
-       $this->privateMethod();
-    }
-
-
+    // public function enginePowerdBy(){
+    //     echo "Battery";
+    // }
 }
-//object 
-$bmw = new Car();
 
-$bmw->publicMethod();
-// $bmw->protectedMethod();
-$bmw->callPrivateMethod();
+$tesla =new Tesla('tesla', 'teslax', 'RED');
 
+$tesla->detail(); 
+// $tesla->enginePowerdBy(); 
 
 
 
-
-
-
-
-
-
-//  $bmw->name= "Bmw Car";
-//$bmw->model= "Bmw-X";
-//  $bmw->detail();
-// echo $bmw->name . '<br/>';
-//echo $bmw->model . '<br/>'; //Error
-// echo $bmw->color . '<br/>'; //Error 
  
 
 
